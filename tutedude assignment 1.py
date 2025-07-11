d8a13478d64602d338ba542e308dd94743f31869
PriyankaSahoo-06 <sahoop2006@gmail.com>
2025-07-11 16:11:58 +0530

    Add files via upload

---
+++ tutedude assignment 1.py
@@ -0,0 +1,12 @@
+# Task 1: Perform Basic Mathematical Operations
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
+#Task 2: Create a Personalized Greeting
+a=input("enter your first name:")
+b=input("enter your last name:")
+print("Hello",a ,b,"! Welcome to Python program.")
